19bd17ec0b30b948e1882fda08a233267cca9927
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-09-12 07:18:45 -0600

    updated files that had not been updated last time

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 Vue.component('tasks', {
   props: ['list'],
-  template: '#tasks-template'
+
+  template: '#tasks-template',
+
+  computed: {
+    remaining: function() {
+      return this.list.filter(this.inProgress).length;
+    }
+  },
+
+  methods: {
+    isCompleted: function(task) {
+      return task.completed;
+    },
+
+    inProgress: function(task) {
+      return !this.isCompleted(task)
+    },
+
+    deleteTask: function(task) {
+      this.list.$remove(task);
+    },
+
+    clearCompleted: function() {
+      this.list = this.list.filter(this.inProgress);
+    }
+  }
 });
 
 new Vue ({
