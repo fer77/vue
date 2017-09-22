a87b15c6b5ab108eace0399448b83406042abedd
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-09-22 07:30:43 -0600

    set up for next task

---
+++ vueJS/eight.js
@@ -0,0 +1,20 @@
+Vue.component('tasks', {
+    props: ['list'],
+    template: '#tasks-template'
+});
+  
+new Vue({
+    el: '#app',
+    data: {
+        tasks: [
+            { body: 'Got to store', completed: false },
+            { body: 'Go to bank', completed: false },
+            { body: 'Go to the doctor', completed: true }
+        ]
+    }
+    // methods: {
+    //     toggleDone: function (task) {
+    //         task.completed = !task.completed;
+    //     }
+    // }
+});
