c5d7a8c9043713b41ba97cf237d1fa2a262e8bd3
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-09-22 07:31:28 -0600

    created methods to count tasks done vs in progress

--- vueJS/main.js
+++ vueJS/main.js
@@ -1,6 +1,25 @@
 Vue.component('tasks', {
     props: ['list'],
-    template: '#tasks-template'
+    template: '#tasks-template',
+    computed: {
+        remaining: function () {
+            return this.list.filter(this.inProgress).length;
+        }
+    },
+    methods: {
+        isCompleted: function (task) {
+            return task.completed;
+        },
+        inProgress: function (task) {
+            return !task.completed;
+        },
+        deleteTask: function (task) {
+            this.list.$remove(task);
+        },
+        clearCompleted: function () {
+            this.list = this.list.filter(this.inProgress);
+        }
+    }
 });
   
 new Vue({
@@ -12,9 +31,4 @@ new Vue({
             { body: 'Go to the doctor', completed: true }
         ]
     }
-    // methods: {
-    //     toggleDone: function (task) {
-    //         task.completed = !task.completed;
-    //     }
-    // }
 });
