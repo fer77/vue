e14d4ae0106c87916fc8eccff7ef117a9c8ed0c0
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-10-03 08:43:27 -0600

    exersice: build an alert component

--- vueJS/main.js
+++ vueJS/main.js
@@ -1,34 +1,24 @@
-Vue.component('tasks', {
-    props: ['list'],
-    template: '#tasks-template',
-    computed: {
-        remaining: function () {
-            return this.list.filter(this.inProgress).length;
-        }
-    },
-    methods: {
-        isCompleted: function (task) {
-            return task.completed;
-        },
-        inProgress: function (task) {
-            return !task.completed;
-        },
-        deleteTask: function (task) {
-            this.list.$remove(task);
-        },
-        clearCompleted: function () {
-            this.list = this.list.filter(this.inProgress);
-        }
-    }
+Vue.component('alert', {
+	template: '#alert-template',
+	props: ['type'],
+	data: function() {
+		return {
+			show: true
+		};
+	},
+	computed: {
+		alertClasses: function() {
+			var type = this.type;
+
+			return {
+				'Alert': true,
+				'Alert--Success': type == 'success',
+				'Alert--Error': type == 'error'
+			}
+		}
+	}
 });
-  
+
 new Vue({
-    el: '#app',
-    data: {
-        tasks: [
-            { body: 'Got to store', completed: false },
-            { body: 'Go to bank', completed: false },
-            { body: 'Go to the doctor', completed: true }
-        ]
-    }
+	el: '#app'
 });
