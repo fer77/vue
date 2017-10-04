7ba7a2a15ea8ab6f1b1fe89188cc93ebf360ddd8
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-10-04 08:08:56 -0600

    created two filters

--- vueJS/main.js
+++ vueJS/main.js
@@ -1,24 +1,15 @@
-Vue.component('alert', {
-	template: '#alert-template',
-	props: ['type'],
-	data: function() {
-		return {
-			show: true
-		};
-	},
-	computed: {
-		alertClasses: function() {
-			var type = this.type;
-
-			return {
-				'Alert': true,
-				'Alert--Success': type == 'success',
-				'Alert--Error': type == 'error'
-			}
-		}
-	}
-});
-
 new Vue({
-	el: '#app'
+	el: '#app',
+	filters: {
+    capitalize: function(value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    },
+    uppercase: function(value) {
+    	if (!value) return ''
+    	value = value.toString()
+    	return value.toUpperCase()
+    }
+  }
 });
