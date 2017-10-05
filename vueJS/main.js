47dbd7648a5fbaf2fe5d5c44bd00d7e52049f7b8
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-10-05 08:44:46 -0600

    , ,

--- vueJS/main.js
+++ vueJS/main.js
@@ -1,15 +1,46 @@
-new Vue({
-	el: '#app',
-	filters: {
-    capitalize: function(value) {
-      if (!value) return ''
-      value = value.toString()
-      return value.charAt(0).toUpperCase() + value.slice(1)
-    },
-    uppercase: function(value) {
-    	if (!value) return ''
-    	value = value.toString()
-    	return value.toUpperCase()
+Vue.component('message', {
+  template: '<input v-model="message" @keyup.enter="storeMessage">',
+
+  data: function() {
+    return { message: '' };
+  },
+
+  methods: {
+    storeMessage: function() {
+      // console.log('Storing ' + this.message);
+
+      newMessage.$emit('new-message', this.message);
+
+      this.message = '';
     }
   }
+});
+
+var newMessage = new Vue();
+
+new Vue({
+	el: '#app',
+
+  data: {
+    messages: []
+  },
+
+  created: function () {
+    newMessage.$on('new-message', this.sendNewMessage);
+    newMessage.$on('new-message', this.handleNewMessage);
+  },
+    methods: {
+    sendNewMessage: function(message) {
+      console.log('Parent is handling ' + message);
+    },
+    handleNewMessage: function(message) {
+      console.log('Parent handled the ' + message);
+      this.messages.push(message);
+    }
+  }
+  // events: {
+    // 'new-message': function(message) {
+    //   console.log('Parent is handling ' + message);
+  //   }
+  // }
 });
