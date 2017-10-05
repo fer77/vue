16c2aa5fc6ba68c5a5b30281101d6fddfe230322
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-10-05 08:43:33 -0600

    set up for next exercise

---
+++ vueJS/fourteen.js
@@ -0,0 +1,15 @@
+new Vue({
+	el: '#app',
+	filters: {
+    capitalize: function(value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    },
+    uppercase: function(value) {
+    	if (!value) return ''
+    	value = value.toString()
+    	return value.toUpperCase()
+    }
+  }
+});
