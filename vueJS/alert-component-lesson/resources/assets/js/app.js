c94458cfaf86ce88a297ac033ea5766137b10434
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-10-16 11:01:49 -0600

    build files

--- vueJS/alert-component-lesson/resources/assets/js/app.js
+++ vueJS/alert-component-lesson/resources/assets/js/app.js
@@ -15,8 +15,13 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+// Vue.component('Alert', require('./components/Alert.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+
+    components: {
+      'Alert': require('./components/Alert.vue')
+    }
 });
