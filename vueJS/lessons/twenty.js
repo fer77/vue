b7f0b5c2c9f2951fb9ee7e27fefe9aabf21d7221
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-10-16 07:23:08 -0600

    set up for next lesson

---
+++ vueJS/lessons/twenty.js
@@ -0,0 +1,14 @@
+Vue.component('home-page', {
+	template: '<h2>Home Page</h2>'
+});
+
+Vue.component('about-page', {
+	template: '<h2>About Page</h2>'
+});
+
+new Vue({
+	el: '#app',
+	data: {
+		currentView: 'home-page'
+	}
+});
