ce7612c3233e4c1641f3a9810f147d0f25bbe6cc
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-10-30 07:43:07 -0600

    child components

--- vue2/main.js
+++ vue2/main.js
@@ -1,6 +1,26 @@
+Vue.component('task-list', {
+  template:
+  `<div>
+      <task v-for="task in tasks">{{ task.task }}</task>
+    </div>`,
+  
+  data() {
+    return {
+      tasks: [
+        { task: 'go to store', complete: true },
+        { task: 'pay bills', complete: false },
+        { task: 'go to bank', complete: false },
+        { task: 'some other task', complete: false },
+        { task: 'some other task 2', complete: true },
+      ]
+    }
+  }
+});
+
 Vue.component('task', {
   template: '<li><slot></slot></li>'
 });
+
 var app = new Vue({
   el: '#root'
 });
