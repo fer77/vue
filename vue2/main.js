4cbcb318aa84e0e01a60c054b69d564a881d30d6
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-11-01 07:48:22 -0600

    created a new modal component

--- vue2/main.js
+++ vue2/main.js
@@ -1,22 +1,20 @@
-Vue.component('message', {
-  props: ['title', 'body'],
-  data() {
-    return {
-      isVisible: true
-    };
-  },
-  template:
-  `<article class="message" v-show="isVisible">
-      <div class="message-header">
-        <p>{{ title }}</p>
-        <button class="delete" aria-label="delete" @click="isVisible = false"></button>
+Vue.component('modal', {
+  template: `
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div class="box">
+        <slot></slot>
       </div>
-      <div class="message-body">
-        {{ body }}
-      </div>
-    </article>`
+    </div>
+    <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+  </div>
+  `
 });
 
 var app = new Vue({
-  el: '#root'
+  el: '#root',
+  data: {
+    showModal: false
+  }
 });
