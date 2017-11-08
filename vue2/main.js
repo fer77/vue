c76a1b71a3673c6747b4b237a53d0129bee9f22b
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-11-08 09:25:07 -0700

    inline-templates

--- vue2/main.js
+++ vue2/main.js
@@ -1,25 +1,7 @@
-Vue.component('modal', {
-  template: `
-  <div class="modal is-active">
-    <div class="modal-background"></div>
-    <div class="modal-card">
-      <header class="modal-card-head">
-        <p class="modal-card-title">
-          <slot name="header"></slot>
-        </p>
-        <button class="delete" aria-label="close"></button>
-      </header>
-      <section class="modal-card-body">
-        <slot>Default text can go here and be replaced in the component's body later.</slot>
-      </section>
-      <footer class="modal-card-foot">
-        <slot name="footer">
-          <button class="button">Sign Up</button>
-        </slot>
-      </footer>
-    </div>
-  </div>
-  `
+Vue.component('progress-view', {
+  data() {
+    return { completionRate: 10 };
+  }
 });
 
 var app = new Vue({
