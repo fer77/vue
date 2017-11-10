67ae7c86ec5063807d20d15db3b75da84972d901
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-11-10 08:16:22 -0700

    created a counter component to test hot reloading

---
+++ vue2/lesson/src/components/Counter.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+    <h2>Counter is: {{ count }}</h2>
+    <button @click="count += 1">Increment counter</button>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+      return { count: 0 }
+  }
+}
+</script>
