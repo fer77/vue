2133cef9385a41adb025a428a393b9456c0f5d2a
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-11-17 09:40:38 -0700

    deleted unneeded files

--- vue2/Vue-Forms/public/js/app.js
+++ vue2/Vue-Forms/public/js/app.js
@@ -1,212 +1,317 @@
-class Errors {
+webpackJsonp([0],{
+
+/***/ 11:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+/* harmony default export */ exports["a"] = {
+	template: '<h1>HELLO WORLD</h1>'
+};
+
+/***/ },
+
+/***/ 12:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__Errors__ = __webpack_require__(30);
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+
+
+var Form = function () {
+    /**
+     * Create a new Form instance.
+     *
+     * @param {object} data
+     */
+    function Form(data) {
+        _classCallCheck(this, Form);
+
+        this.originalData = data;
+
+        for (var field in data) {
+            this[field] = data[field];
+        }
+
+        this.errors = new __WEBPACK_IMPORTED_MODULE_0__Errors__["a" /* default */]();
+    }
+
+    /**
+     * Fetch all relevant data for the form.
+     */
+
+
+    _createClass(Form, [{
+        key: 'data',
+        value: function data() {
+            var data = {};
+
+            for (var property in this.originalData) {
+                data[property] = this[property];
+            }
+
+            return data;
+        }
+
+        /**
+         * Reset the form fields.
+         */
+
+    }, {
+        key: 'reset',
+        value: function reset() {
+            for (var field in this.originalData) {
+                this[field] = '';
+            }
+
+            this.errors.clear();
+        }
+
+        /**
+         * Send a POST request to the given URL.
+         * .
+         * @param {string} url
+         */
+
+    }, {
+        key: 'post',
+        value: function post(url) {
+            return this.submit('post', url);
+        }
+
+        /**
+         * Send a PUT request to the given URL.
+         * .
+         * @param {string} url
+         */
+
+    }, {
+        key: 'put',
+        value: function put(url) {
+            return this.submit('put', url);
+        }
+
+        /**
+         * Send a PATCH request to the given URL.
+         * .
+         * @param {string} url
+         */
+
+    }, {
+        key: 'patch',
+        value: function patch(url) {
+            return this.submit('patch', url);
+        }
+
+        /**
+         * Send a DELETE request to the given URL.
+         * .
+         * @param {string} url
+         */
+
+    }, {
+        key: 'delete',
+        value: function _delete(url) {
+            return this.submit('delete', url);
+        }
+
+        /**
+         * Submit the form.
+         *
+         * @param {string} requestType
+         * @param {string} url
+         */
+
+    }, {
+        key: 'submit',
+        value: function submit(requestType, url) {
+            var _this = this;
+
+            return new Promise(function (resolve, reject) {
+                axios[requestType](url, _this.data()).then(function (response) {
+                    _this.onSuccess(response.data);
+
+                    resolve(response.data);
+                }).catch(function (error) {
+                    _this.onFail(error.response.data);
+
+                    reject(error.response.data);
+                });
+            });
+        }
+
+        /**
+         * Handle a successful form submission.
+         *
+         * @param {object} data
+         */
+
+    }, {
+        key: 'onSuccess',
+        value: function onSuccess(data) {
+            alert(data.message); // temporary
+
+            this.reset();
+        }
+
+        /**
+         * Handle a failed form submission.
+         *
+         * @param {object} errors
+         */
+
+    }, {
+        key: 'onFail',
+        value: function onFail(errors) {
+            this.errors.record(errors);
+        }
+    }]);
+
+    return Form;
+}();
+
+/* harmony default export */ exports["a"] = Form;
+
+/***/ },
+
+/***/ 30:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Errors = function () {
     /**
      * Create a new Errors instance.
      */
-    constructor() {
+    function Errors() {
+        _classCallCheck(this, Errors);
+
         this.errors = {};
     }
-
 
     /**
      * Determine if an errors exists for the given field.
      *
      * @param {string} field
      */
-    has(field) {
-        return this.errors.hasOwnProperty(field);
-    }
 
 
-    /**
-     * Determine if we have any errors.
-     */
-    any() {
-        return Object.keys(this.errors).length > 0;
-    }
-
-
-    /**
-     * Retrieve the error message for a field.
-     *
-     * @param {string} field
-     */
-    get(field) {
-        if (this.errors[field]) {
-            return this.errors[field][0];
-        }
-    }
-
-
-    /**
-     * Record the new errors.
-     *
-     * @param {object} errors
-     */
-    record(errors) {
-        this.errors = errors;
-    }
-
-
-    /**
-     * Clear one or all error fields.
-     *
-     * @param {string|null} field
-     */
-    clear(field) {
-        if (field) {
-            delete this.errors[field];
-
-            return;
+    _createClass(Errors, [{
+        key: "has",
+        value: function has(field) {
+            return this.errors.hasOwnProperty(field);
         }
 
-        this.errors = {};
-    }
-}
+        /**
+         * Determine if we have any errors.
+         */
 
-
-class Form {
-    /**
-     * Create a new Form instance.
-     *
-     * @param {object} data
-     */
-    constructor(data) {
-        this.originalData = data;
-
-        for (let field in data) {
-            this[field] = data[field];
+    }, {
+        key: "any",
+        value: function any() {
+            return Object.keys(this.errors).length > 0;
         }
 
-        this.errors = new Errors();
-    }
+        /**
+         * Retrieve the error message for a field.
+         *
+         * @param {string} field
+         */
 
-
-    /**
-     * Fetch all relevant data for the form.
-     */
-    data() {
-        let data = {};
-
-        for (let property in this.originalData) {
-            data[property] = this[property];
+    }, {
+        key: "get",
+        value: function get(field) {
+            if (this.errors[field]) {
+                return this.errors[field][0];
+            }
         }
 
-        return data;
-    }
+        /**
+         * Record the new errors.
+         *
+         * @param {object} errors
+         */
 
-
-    /**
-     * Reset the form fields.
-     */
-    reset() {
-        for (let field in this.originalData) {
-            this[field] = '';
+    }, {
+        key: "record",
+        value: function record(errors) {
+            this.errors = errors;
         }
 
-        this.errors.clear();
-    }
+        /**
+         * Clear one or all error fields.
+         *
+         * @param {string|null} field
+         */
+
+    }, {
+        key: "clear",
+        value: function clear(field) {
+            if (field) {
+                delete this.errors[field];
+
+                return;
+            }
+
+            this.errors = {};
+        }
+    }]);
+
+    return Errors;
+}();
+
+/* harmony default export */ exports["a"] = Errors;
+
+/***/ },
+
+/***/ 34:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_vue__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_axios__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_axios___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_axios__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__core_Form__ = __webpack_require__(12);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__components_Example__ = __webpack_require__(11);
 
 
-    /**
-     * Send a POST request to the given URL.
-     * .
-     * @param {string} url
-     */
-    post(url) {
-        return this.submit('post', url);
-    }
 
 
-    /**
-     * Send a PUT request to the given URL.
-     * .
-     * @param {string} url
-     */
-    put(url) {
-        return this.submit('put', url);
-    }
 
 
-    /**
-     * Send a PATCH request to the given URL.
-     * .
-     * @param {string} url
-     */
-    patch(url) {
-        return this.submit('patch', url);
-    }
+// Since we'll want to reference these two in other files as 'new Form':
+window.axios = __WEBPACK_IMPORTED_MODULE_1_axios___default.a;
+window.Form = __WEBPACK_IMPORTED_MODULE_2__core_Form__["a" /* default */];
 
-
-    /**
-     * Send a DELETE request to the given URL.
-     * .
-     * @param {string} url
-     */
-    delete(url) {
-        return this.submit('delete', url);
-    }
-
-
-    /**
-     * Submit the form.
-     *
-     * @param {string} requestType
-     * @param {string} url
-     */
-    submit(requestType, url) {
-        return new Promise((resolve, reject) => {
-            axios[requestType](url, this.data())
-                .then(response => {
-                    this.onSuccess(response.data);
-
-                    resolve(response.data);
-                })
-                .catch(error => {
-                    this.onFail(error.response.data);
-
-                    reject(error.response.data);
-                });
-        });
-    }
-
-
-    /**
-     * Handle a successful form submission.
-     *
-     * @param {object} data
-     */
-    onSuccess(data) {
-        alert(data.message); // temporary
-
-        this.reset();
-    }
-
-
-    /**
-     * Handle a failed form submission.
-     *
-     * @param {object} errors
-     */
-    onFail(errors) {
-        this.errors.record(errors);
-    }
-}
-
-
-new Vue({
+new __WEBPACK_IMPORTED_MODULE_0_vue___default.a({
     el: '#app',
-
+    components: {
+        Example: __WEBPACK_IMPORTED_MODULE_3__components_Example__["a" /* default */] // <example></example>
+    },
     data: {
-        form: new Form({
+        form: new __WEBPACK_IMPORTED_MODULE_2__core_Form__["a" /* default */]({
             name: '',
             description: ''
         })
     },
 
     methods: {
-        onSubmit() {
-            this.form.post('/projects')
-                .then(response => alert('Wahoo!'));
+        onSubmit: function onSubmit() {
+            this.form.post('/projects').then(function (response) {
+                return alert('Wahoo!');
+            });
         }
     }
 });
+
+/***/ }
+
+},[34]);
